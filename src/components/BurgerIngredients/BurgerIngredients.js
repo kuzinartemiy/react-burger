2f90b36c401bba925a5ddc60b90eca1232091498
Artemiy Kuzin <kuzin_artemiy@list.ru>
2022-01-14 13:48:24 +0500

    add smooth scroll in tabs

--- src/components/BurgerIngredients/BurgerIngredients.js
+++ src/components/BurgerIngredients/BurgerIngredients.js
@@ -1,56 +1,44 @@
 import styles from './BurgerIngredients.module.css';
 import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { IngredientsList } from '../IngredientsList/IngredientsList';
 import PropTypes from 'prop-types';
 import { IngredientType } from '../../utils/types';
 
-export const BurgerIngredients = ({ingredients, openModal}) => {
+export const BurgerIngredients = ({ ingredients, openModal }) => {
   const [current, setCurrent] = useState('buns');
 
-  const buns = ingredients.filter(ingredient => ingredient.type === 'bun');
-  const sauces = ingredients.filter(ingredient => ingredient.type === 'sauce');
-  const mains = ingredients.filter(ingredient => ingredient.type === 'main');
+  const clickTabHandler = (value) => {
+    const activeIngredientList = document.getElementById(value);
+    activeIngredientList.scrollIntoView({ behavior: 'smooth'});
+    setCurrent(value);
+  }
+
+  const buns = useMemo(() => ingredients.filter(ingredient => ingredient.type === 'bun'), [ingredients]);
+  const sauces = useMemo(() => ingredients.filter(ingredient => ingredient.type === 'sauce'), [ingredients]);
+  const mains = useMemo(() => ingredients.filter(ingredient => ingredient.type === 'main'), [ingredients]);
 
   return (
     <div className={styles.burgerIngredients}>
       <h1 className="text text_type_main-large test">Соберите бургер</h1>
-      <div className={styles.burgerIngredients__tabs} style={{ display: 'flex' }}>
-        <Tab value="buns" active={current === 'buns'} onClick={setCurrent}>
+      
+      <div className={styles.burgerIngredients__tabs}>
+        <Tab value="buns" active={current === 'buns'} onClick={clickTabHandler}>
           Булки
         </Tab>
-        <Tab value="sauces" active={current === 'sauces'} onClick={setCurrent}>
+        <Tab value="sauces" active={current === 'sauces'} onClick={clickTabHandler}>
           Соусы
         </Tab>
-        <Tab value="mains" active={current === 'mains'} onClick={setCurrent}>
+        <Tab value="mains" active={current === 'mains'} onClick={clickTabHandler}>
           Начинки
         </Tab>
       </div>
-      
-      {current === 'buns' && 
-        <div className={styles.burgerIngredients__tabsContent}>
-          <IngredientsList openModal={openModal} title="Булки" ingredients={buns} />
-          <IngredientsList openModal={openModal} title="Соусы" ingredients={sauces} />
-          <IngredientsList openModal={openModal} title="Начинки" ingredients={mains} />
-        </div>
-      }
-      
-      {current === 'sauces' && 
-        <div className={styles.burgerIngredients__tabsContent}>
-          <IngredientsList openModal={openModal} title="Соусы" ingredients={sauces} />
-          <IngredientsList openModal={openModal} title="Начинки" ingredients={mains} />
-          <IngredientsList openModal={openModal} title="Булки" ingredients={buns} />
-        </div>
-      }
 
-      {current === 'mains' && 
-        <div className={styles.burgerIngredients__tabsContent}>
-          <IngredientsList openModal={openModal} title="Начинки" ingredients={mains} />
-          <IngredientsList openModal={openModal} title="Булки" ingredients={buns} />
-          <IngredientsList openModal={openModal} title="Соусы" ingredients={sauces} />
-        </div>
-      }
-
+      <div className={styles.burgerIngredients__tabsContent}>
+        <IngredientsList openModal={openModal} title="Булки" ingredients={buns} />
+        <IngredientsList openModal={openModal} title="Соусы" ingredients={sauces} />
+        <IngredientsList openModal={openModal} title="Начинки" ingredients={mains} />
+      </div>
     </div>
   )
 }
@@ -59,4 +47,3 @@ BurgerIngredients.propTypes = {
   ingredients: PropTypes.arrayOf(PropTypes.shape(IngredientType).isRequired).isRequired,
   openModal: PropTypes.func.isRequired,
 };
-
